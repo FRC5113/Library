2dfa79dab42b90d59fc8bfc02801daf59c7e2148
Vladimir Bondar (Lemons) <33793511+drvladb@users.noreply.github.com>
2022-12-25 16:47:02 -0500

    Button comments

--- src/main/java/com/frc5113/library/oi/buttons/logic/ButtonAnd.java
+++ src/main/java/com/frc5113/library/oi/buttons/logic/ButtonAnd.java
@@ -2,6 +2,9 @@ package com.frc5113.library.oi.buttons.logic;
 
 import edu.wpi.first.wpilibj2.command.button.Button;
 
+/**
+ * Treat two buttons as one (where the fake button is "pressed" when both real buttons are pressed)
+ */
 public class ButtonAnd extends Button {
     Button b1;
     Button b2;
--- src/main/java/com/frc5113/library/oi/buttons/logic/ButtonOr.java
+++ src/main/java/com/frc5113/library/oi/buttons/logic/ButtonOr.java
@@ -2,6 +2,9 @@ package com.frc5113.library.oi.buttons.logic;
 
 import edu.wpi.first.wpilibj2.command.button.Button;
 
+/**
+ * Treat two buttons as one (where the fake button is "pressed" when either of the real buttons is pressed)
+ */
 public class ButtonOr extends Button {
     Button b1;
     Button b2;
