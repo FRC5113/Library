8772f472afe85428e9d672d888a8cea163e836b4
Vlad Bondar <vlad@vladb.xyz>
2023-01-28 19:01:37 -0500

    fix formatting

--- src/main/java/com/frc5113/library/drivers/OctonumModule.java
+++ src/main/java/com/frc5113/library/drivers/OctonumModule.java
@@ -1,48 +1,46 @@
 package com.frc5113.library.drivers;
 
-import edu.wpi.first.wpilibj.DoubleSolenoid;
+import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
 
 import com.frc5113.library.motors.SmartFalcon;
 import com.frc5113.library.primative.OctonumType;
-
-import static edu.wpi.first.wpilibj.DoubleSolenoid.Value.*;
-
+import edu.wpi.first.wpilibj.DoubleSolenoid;
 
 public class OctonumModule {
-    public SmartFalcon motor;
-    public DoubleSolenoid solenoid;
+  public SmartFalcon motor;
+  public DoubleSolenoid solenoid;
 
-    public OctonumType current;
+  public OctonumType current;
 
-    public OctonumModule(SmartFalcon motor, DoubleSolenoid solenoid, OctonumType startState) {
-        this.motor = motor;
-        this.solenoid = solenoid;
-        current = startState;
+  public OctonumModule(SmartFalcon motor, DoubleSolenoid solenoid, OctonumType startState) {
+    this.motor = motor;
+    this.solenoid = solenoid;
+    current = startState;
 
-        if(current == OctonumType.tank) {
-            solenoid.set(kForward);
-        } else {
-            solenoid.set(kReverse);
-        }
+    if (current == OctonumType.tank) {
+      solenoid.set(kForward);
+    } else {
+      solenoid.set(kReverse);
     }
+  }
 
-    public void set(OctonumType type) {
-        if(current != type) {
-            solenoid.toggle();
-        }
-        current = type;
+  public void set(OctonumType type) {
+    if (current != type) {
+      solenoid.toggle();
     }
-    
-    public void toggle() {
-        solenoid.toggle();
-    }
+    current = type;
+  }
 
-    public void solenoidOff() {
-        solenoid.set(kOff);
-    }
+  public void toggle() {
+    solenoid.toggle();
+  }
 
-    public void brake() {
-        solenoid.set(kForward);
-        motor.stopMotor();
-    }
+  public void solenoidOff() {
+    solenoid.set(kOff);
+  }
+
+  public void brake() {
+    solenoid.set(kForward);
+    motor.stopMotor();
+  }
 }
