70658ed55761c496fb9ef92120c63862bf318b84
Vlad B <33793511+drvladb@users.noreply.github.com>
2023-03-09 13:34:39 -0800

    Make SubsystemManager great again (#21)
    * BREAKING: Add looping code / modify SmartSubsystem
    
    * BREAKING: make subsystems abstract methods
    
    * fix build issues by updating example subsystems
    
    * BREAKING > Add periodic subsystem checks and separate looper.
    
    * Fix formatting issues.
    
    * Implemented new check loop inside library-provided subsystems.
    
    ---------
    
    Co-authored-by: Kevin Hwang <timmyhwang127@gmail.com>

---
+++ src/main/java/com/frc5113/library/loops/ILooper.java
@@ -0,0 +1,5 @@
+package com.frc5113.library.loops;
+
+public interface ILooper {
+  void register(Loop loop);
+}
--- src/main/java/com/frc5113/library/subsystem/SmartSubsystem.java
+++ src/main/java/com/frc5113/library/subsystem/SmartSubsystem.java
@@ -1,5 +1,6 @@
 package com.frc5113.library.subsystem;
 
+import com.frc5113.library.loops.ILooper;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /**
@@ -23,9 +24,39 @@ public abstract class SmartSubsystem extends SubsystemBase {
    */
   public abstract boolean checkSubsystem();
 
+  /**
+   * Adds functions to be executed at the start, periodic, and end of the robot life cycle. Used to
+   * perform simple checks to verify the correct operation of a subsystem. Should the checks fail,
+   * errors must be reported.
+   */
+  public abstract void registerPeriodicSubsystemCheck(ILooper mCheckLooper);
+
+  /**
+   * Have the subsystem perform a simple and quickly executing check that verifies the correct
+   * operation of the subsystem. Should print debug data if something fails
+   *
+   * @return Check status (success - true / fail - false)
+   */
+  public abstract boolean checkSubsystemPeriodic();
+
   /** Stop everything in the subsystem (now) */
   public abstract void stop();
 
   /** Reset all the motors */
   public abstract void zeroSensors();
+
+  /**
+   * Add functions to be executed at the start, periodic, and end of the robot lifecycle. Optional.
+   */
+  public abstract void registerEnabledLoops(ILooper mEnabledLooper);
+
+  /** Read information from constricted resources (ie CAN) */
+  public abstract void readPeriodicInputs();
+
+  /** Write periodic information to constricted resources (ie CAN) */
+  public abstract void writePeriodicOutputs();
+
+  // Periodic doesn't need to exist
+  @Override
+  public void periodic() {}
 }
